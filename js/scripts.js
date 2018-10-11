f9ed24d0a1a53548c2bc3795fe02dc7b4d53209b
Zachjrivera <Zachjrivera@gmail.com>
2018-10-11 09:08:35 -0400

    Homework

---
+++ js/scripts.js
@@ -0,0 +1,13 @@
+ function visitPage(){
+        window.location='https://www.youtube.com/watch?v=ig1W48LjI0E';
+    }
+
+   function welcome(){
+ 		alert("Welcome sukka")
+ 	}
+    	
+    	
+ 
+ 	
+ 	
+   
